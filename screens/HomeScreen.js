afe2f0fce9dc258fd4acb1b208886968796e8ea2
Rahul Nikam <codewithrahulnikam@gmail.com>
2022-07-14 01:28:42 +0530

    Food Delivery App UI

---
+++ screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import tw from 'tailwind-react-native-classnames'
+
+const HomeScreen = () => {
+  return (
+    <View style={tw`pt-10 px-5`}>
+      <Text>HomeScreen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
